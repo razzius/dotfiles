339d307bfb06f3550ee4121bece4e3ece7f226d5
Razzi Abuissa <razzi53@gmail.com>
2018-04-11 12:17:54 -0700

    Add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    // enable additional rules
+    "quotes": ["error", "single"],
+    "semi": [2, "never"],
+
+    // override default options for rules from base configurations
+    // "comma-dangle": ["error", "always"],
+    "no-cond-assign": ["error", "always"],
+
+    "no-console": "off",
+    "strict": 0
+  }
+}
