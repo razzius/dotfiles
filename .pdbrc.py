bdebd5e96abcf81132b65f109f6fb8733d18a5f8
Razzi Abuissa <razzi53@gmail.com>
2019-10-31 09:49:56 -0700

    gitconfig and .pdbrc.py

--- .pdbrc.py
+++ .pdbrc.py
@@ -1,6 +1,25 @@
+import atexit
+import os
+import readline
+
 from pdb import DefaultConfig
 
+
 class Config(DefaultConfig):
-    prompt = '\033[34;1;4m+pdb>\033[0m '
+    prompt = '\x1b[34m' + '+pdb> ' + '\x1b[0m'
     sticky_by_default = True
     editor = 'emacsclient -n'
+
+
+historyPath = os.path.expanduser("~/.pyhistory")
+
+
+def save_history(historyPath=historyPath):
+    readline.write_history_file(historyPath)
+
+
+if os.path.exists(historyPath):
+    readline.read_history_file(historyPath)
+
+
+atexit.register(save_history, historyPath=historyPath)
