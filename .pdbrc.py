8909cca62b6402b70633c534908394fab5a58af0
Razzi Abuissa <razzi53@gmail.com>
2019-11-13 15:21:35 -0800

    Add commented out history pdb++ config with disabled pygments

--- .pdbrc.py
+++ .pdbrc.py
@@ -1,25 +1,31 @@
-import atexit
-import os
-import readline
+# import atexit
+# import os
+# import readline
+# try:
+#     interact
+# except:
+#     pass
 
 from pdb import DefaultConfig
+# from pygments.formatters import terminal
 
 
 class Config(DefaultConfig):
     prompt = '+pbd> '
     sticky_by_default = True
     editor = 'emacsclient -n'
+    use_pygments = False
 
 
-historyPath = os.path.expanduser("~/.pyhistory")
+# historyPath = os.path.expanduser("~/.pyhistory")
 
 
-def save_history(historyPath=historyPath):
-    readline.write_history_file(historyPath)
+# def save_history(historyPath=historyPath):
+#     readline.write_history_file(historyPath)
 
 
-if os.path.exists(historyPath):
-    readline.read_history_file(historyPath)
+# if os.path.exists(historyPath):
+#     readline.read_history_file(historyPath)
 
 
-atexit.register(save_history, historyPath=historyPath)
+# atexit.register(save_history, historyPath=historyPath)
